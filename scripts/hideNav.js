eb749e2f9c4354451c2ab78062ddd6c05e5030f0
Bill <eid20021@gmail.com>
2022-09-29 12:14:44 +0300

    fix: use best web language

--- scripts/hideNav.js
+++
@@ -1,9 +0,0 @@
-function hideNav(navId, gridId) {
-    if (!document.getElementById(navId).classList.contains("nav_hidden")) {
-        document.getElementById(navId).classList.add("nav_hidden");
-        document.getElementById(gridId).classList.add("body_no-nav");
-        return;
-    }
-    document.getElementById(navId).classList.remove("nav_hidden");
-    document.getElementById(gridId).classList.remove("body_no-nav");
-}
